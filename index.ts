945d9321fb4493b504a81fe0a372bc4ad891c584
Deathonyou <158500977+Deathonyou@users.noreply.github.com>
2024-02-07 18:55:00 +0000

    adding a dugeon generation

--- index.ts
+++ index.ts
@@ -1,35 +1,3 @@
-console.log("Hello, Node.js!");
-import * as commander from 'commander';
+import { Console } from "console";
 
-const program = new commander.Command();
-program.version('1.0.0');
-
-
-
-program.parse(process.argv);
-program
-  .command('say-hello')
-  .description('Prints a friendly greeting')
-  .action(() => {
-    console.log('Hello, world!');
-  });
-
-program.parse(process.argv);
-function showProgress() {
-    const progressBarLength = 30;
-    let progress = 0;
-  
-    const interval = setInterval(() => {
-      const percentage = (progress / 100) * progressBarLength;
-      const progressBar = '='.repeat(percentage) + ' '.repeat(progressBarLength - percentage);
-      process.stdout.write(`[${progressBar}] ${progress}%\r`);
-  
-      progress += 10;
-      if (progress > 100) {
-        clearInterval(interval);
-        console.log('\nProcess complete!');
-      }
-    }, 1000);
-  }
-  
-  showProgress();
+Console
